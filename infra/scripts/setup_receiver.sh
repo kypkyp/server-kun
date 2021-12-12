146315c03e628faba7572ae743fbd7d95b137c03
kyp <kyp@kmc.gr.jp>
2021-12-12 18:26:05 +0900

    systemctlでデーモン化

--- infra/scripts/setup_receiver.sh
+++ infra/scripts/setup_receiver.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Create .env file used by receiver exec
 cat << EOF > /.env
 DISCORD_TOKEN=${discord_token}
 DISCORD_CHANNEL=${discord_channel}
@@ -9,7 +10,27 @@ START_MESSAGE=${start_message}
 STOP_MESSAGE=${stop_message}
 EOF
 
+# Create systemctl file
+cat << EOF > /etc/systemd/system/server-kun.service
+[Unit]
+Description = server-kun
+After = network-online.target
+
+[Service]
+ExecStart = /receiver
+WorkingDirectory = /
+
+User = root
+Restart = always
+
+Type = simple
+
+[Install]
+WantedBy = multi-user.target
+EOF
+
 apt install -y unzip
 curl -L ${zip_url} > /receiver.zip
 unzip /receiver.zip
-/receiver
+sudo systemctl enable server-kun
+sudo systemctl start server-kun
